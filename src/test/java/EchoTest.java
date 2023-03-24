a91c57eaa95411a5ca7c8eb571a835fa6e233731
pricheska <den-ryab2019@yandex.ru>
2023-03-24 14:46:01 +0400

    Make tests

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -1,22 +1,54 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class EchoTest {
     @Test
-    public void Test() {
+    public void shouldReturnName() {
         // Given - When - Then
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("name:Denis") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-            //    .body()
+                .body("data", equalTo("name:Denis"))
         ;
     }
+    @Test
+    public void shouldReturnId() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:20544") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("id:20544"))
+        ;
+    }
+    @Test
+    void ShouldReturnCurrency () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("USD")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("USD"))
+                ;
+    }
+
 }
