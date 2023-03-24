7b5ee3b16a94dc5af02ae5aa818657c3a0f53080
pricheska <den-ryab2019@yandex.ru>
2023-03-24 16:08:18 +0400

    Fix test and CI

--- src/test/java/EchoTest.java
+++ src/test/java/EchoTest.java
@@ -49,7 +49,7 @@ public class EchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("RUB"))
+                .body("data", equalTo("USD"))
                 ;
     }
 
